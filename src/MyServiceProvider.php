be93570ebf34d263629444b5088d8136c15ebb36
chengdi <956591241@qq.com>
2020-06-29 14:28:38 +0800

    cd

--- src/MyServiceProvider.php
+++ src/MyServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace cheng0_0di\norgetamap;
+namespace Norge\Mymap;
 
 use Laravel\Nova\Nova;
 use Laravel\Nova\Events\ServingNova;
--- src/Mymap.php
+++ src/Mymap.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace cheng0_0di\norgetamap;
+namespace Norge\Mymap;
 
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Http\Requests\NovaRequest;
